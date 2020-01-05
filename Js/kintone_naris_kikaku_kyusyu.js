7b287ed68c96bbe5f279f0ec75513e389b331d78
swyroak <947x9i@bma.biglobe.ne.jp>
2020-01-05 17:28:02 +0900

    add kyusyu

--- Js/kintone_naris_kikaku_kyusyu.js
+++ Js/kintone_naris_kikaku_kyusyu.js
@@ -1,96 +1,20 @@
 (function () {
-    kintone.events.on('app.record.detail.show', function (event) {
-        "use strict";
+    kintone.events.on('app.record.index.show', function (event) {
+        'use strict'
         let mybutton = document.createElement('button');
-        let record = event.record
+        let records = event.records;
+        const BASEDAY = new Date('1982/10/20 09:00')
         mybutton.id = 'mybutton';
         mybutton.innerText = 'データコピー';
         mybutton.onclick = function () {
-            let table_data = record['商品詳細']['value']
-            console.log(table_data)
-            table_data.forEach((value) => {
-                let body = {
-                    "app": "47",
-                    "records": [
-                        {
-                            "支所倉庫コード": { "value": record['支所倉庫コード']['value'] },
-                            "荷主コード": { "value": "0514507" },
-                            "商品コード": { "value": value['value']['JANコード']['value'] },
-                            "納品先コード": { "value": record['納品先コード']['value'] },
-                            "納品先分割識別子": { "value": 0 },
-                            "日付": { "value": value['value']['引当1']['value'].replace(/-/g, '') },
-                            "企画回": { "value": record['企画回']['value'] },
-                            "予測数": { "value": Math.ceil(value['value']['数量1']['value']) },
-                            "アバウト数": { "value": 0 },
-                            "ID": { "value": record['納品先コード']['value'] + '_' + record['企画回']['value'] + '_' + value['value']['JANコード']['value'] + '_01' },
-                            "日付シリアル": { "value": value['value']['引当1']['value'] }
-                        },
-                        {
-                            "支所倉庫コード": { "value": record['支所倉庫コード']['value'] },
-                            "荷主コード": { "value": "0514507" },
-                            "商品コード": { "value": value['value']['JANコード']['value'] },
-                            "納品先コード": { "value": record['納品先コード']['value'] },
-                            "納品先分割識別子": { "value": 0 },
-                            "日付": { "value": value['value']['引当2']['value'].replace(/-/g, '') },
-                            "企画回": { "value": record['企画回']['value'] },
-                            "予測数": { "value": Math.ceil(value['value']['数量2']['value']) },
-                            "アバウト数": { "value": 0 },
-                            "ID": { "value": record['納品先コード']['value'] + '_' + record['企画回']['value'] + '_' + value['value']['JANコード']['value'] + '_02' },
-                            "日付シリアル": { "value": value['value']['引当2']['value'] }
-                        },
-                        {
-                            "支所倉庫コード": { "value": record['支所倉庫コード']['value'] },
-                            "荷主コード": { "value": "0514507" },
-                            "商品コード": { "value": value['value']['JANコード']['value'] },
-                            "納品先コード": { "value": record['納品先コード']['value'] },
-                            "納品先分割識別子": { "value": 0 },
-                            "日付": { "value": value['value']['引当3']['value'].replace(/-/g, '') },
-                            "企画回": { "value": record['企画回']['value'] },
-                            "予測数": { "value": Math.ceil(value['value']['数量3']['value']) },
-                            "アバウト数": { "value": 0 },
-                            "ID": { "value": record['納品先コード']['value'] + '_' + record['企画回']['value'] + '_' + value['value']['JANコード']['value'] + '_03' },
-                            "日付シリアル": { "value": value['value']['引当3']['value'] }
-                        },
-                        {
-                            "支所倉庫コード": { "value": record['支所倉庫コード']['value'] },
-                            "荷主コード": { "value": "0514507" },
-                            "商品コード": { "value": value['value']['JANコード']['value'] },
-                            "納品先コード": { "value": record['納品先コード']['value'] },
-                            "納品先分割識別子": { "value": 0 },
-                            "日付": { "value": value['value']['引当4']['value'].replace(/-/g, '') },
-                            "企画回": { "value": record['企画回']['value'] },
-                            "予測数": { "value": Math.ceil(value['value']['数量4']['value']) },
-                            "アバウト数": { "value": 0 },
-                            "ID": { "value": record['納品先コード']['value'] + '_' + record['企画回']['value'] + '_' + value['value']['JANコード']['value'] + '_04' },
-                            "日付シリアル": { "value": value['value']['引当4']['value'] }
-                        },
-                        {
-                            "支所倉庫コード": { "value": record['支所倉庫コード']['value'] },
-                            "荷主コード": { "value": "0514507" },
-                            "商品コード": { "value": value['value']['JANコード']['value'] },
-                            "納品先コード": { "value": record['納品先コード']['value'] },
-                            "納品先分割識別子": { "value": 0 },
-                            "日付": { "value": value['value']['引当5']['value'].replace(/-/g, '') },
-                            "企画回": { "value": record['企画回']['value'] },
-                            "予測数": { "value": Math.ceil(value['value']['数量5']['value']) },
-                            "アバウト数": { "value": 0 },
-                            "ID": { "value": record['納品先コード']['value'] + '_' + record['企画回']['value'] + '_' + value['value']['JANコード']['value'] + '_05' },
-                            "日付シリアル": { "value": value['value']['引当5']['value'] }
-                        }
-                    ]
-                }
-                console.log(body)
-
-                kintone.api(kintone.api.url('/k/v1/records', true), 'POST', body, function (resp) {
-                    // success
-                    console.log(resp);
-                }, function (error) {
-                    // error
-                    console.log(error);
-                    window.alert(error['message'] + '画面をそのままにして誰か呼んでください')
-                });
+            console.log(records);
+            records.forEach((value) => {
+                const TARGETDAY = BASEDAY.getDate() + value['号数']['value']
+                console.log(TARGETDAY)
+                console.log((value['号数']['value'] % 52 - 17) * 7)
             });
+
         }
-        kintone.app.record.getHeaderMenuSpaceElement().appendChild(mybutton);
-    });
+        kintone.app.getHeaderMenuSpaceElement().appendChild(mybutton);
+    })
 })();
